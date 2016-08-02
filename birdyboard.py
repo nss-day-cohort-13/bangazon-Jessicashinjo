40663463a12d3c4f682232e4fee78bfc599e4c63
Jessica Wynn <Jessicashinjo@gmail.com>
2016-08-02 10:40:53 -0500

    Next 2 docstrings written for implementation code

--- birdyboard.py
+++ birdyboard.py
@@ -2,7 +2,7 @@
 class Birdyboard():
 
   def create_user(self, full_name, screen_name):
-    ''' Takes arguements of the users full name and the user's screen name:
+    ''' Takes arguements of the user's full name and the user's screen name:
 
     Checks to see if user_data file exists. If the file does not exist
     it will create the file and add the user. If it does exist, it will read
@@ -13,22 +13,43 @@ class Birdyboard():
     '''
     pass
 
-  def write_chirp(self, full_name, privacy, to_whom, message):
-    ''' Takes arguments of the users full name, if it should be public or
-        private, who the chirp will be sent to, and the message itself:
+  def write_chirp(self, full_name, screen_name, privacy, to_whom, message):
+    ''' Takes arguments of the user's full name, screen name, if it should be
+        public or private, who the chirp will be sent to, and the message
+        itself:
 
     Opens user_data and searches for user. If the file does not contain user,
     throws an error to sign up and sends user back to the menu. If it does
     contain user it will close the file and open the chirps file. Next it will
     check the privacy. If privacy is public it will generate a chirp_id
-    and write the chirp_id, full_name, privacy, to_whom (automatically listed
-    as "public" if privacy is public), and message to the chirps.csv file.
+    and write the chirp_id, full_name, screen_name, privacy, to_whom
+    (automatically listed as "public" if privacy is public), and message to the
+    chirps.csv file.
     '''
     pass
 
-  def read_chirps(self, full_name):
+  def read_chirps(self, screen_name):
+    ''' Takes the user's screen_name as its argument:
 
+    Opens user_data.csv file and searches for the screen_name. If the
+    screen_name does not exist it will only print public chirps. If the
+    screen_name does exist it will print public and the user's private chirps in
+    separate sections.
+    '''
     pass
 
-  def delete_chirp():
+  def delete_chirp(self, screen_name, chirp_id):
+    ''' Takes arguments of the user's screen name and id of the message to be
+        deleted:
+
+    First it will open the user_data.csv and check to see if the owner of the
+    chirp is the one deleting chirp. If the person deleting the chirp is not
+    the owner, it will throw an error saying "You cannot delete another user's
+    chirp". If the person deleting the chirp is the owner, it will remove the
+    chirp (without asking "Are you sure?") and allow the user to continue to
+    delete chirps until they are done.
+    '''
     pass
+
+
+# Separate functions to see if user_exists and screen_name exists?
